521b18a8e95673c5cbacc457d12bfc1ef1ca01e0
David Carter <dajocarter@gmail.com>
2018-08-20 00:12:00 -0700

    Replace inline-styles with styled-components

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,32 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import styled from 'styled-components'
+
+const Container = styled.header`
+  background: rebeccapurple;
+  margin-bottom: 1.45rem;
+`
+const Nav = styled.nav`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`
+const NavBrand = styled.h1`
+  margin: 0;
+`
+const BrandLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+`
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
+  <Container>
+    <Nav>
+      <NavBrand>
+        <BrandLink to="/">{siteTitle}</BrandLink>
+      </NavBrand>
+    </Nav>
+  </Container>
 )
 
 export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,8 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
+import styled from 'styled-components'
 
 import Header from './header'
+
+const Main = styled.main`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0px 1.0875rem 1.45rem;
+  padding-top: 0;
+`
 
 const Layout = ({ children, data }) => (
   <StaticQuery
@@ -28,16 +36,7 @@ const Layout = ({ children, data }) => (
           <html lang="en" />
         </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
+        <Main>{children}</Main>
       </>
     )}
   />
