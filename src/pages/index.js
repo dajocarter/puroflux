17742bad4a03672c847c0ef4da772e9af3df3888
David Carter <dajocarter@gmail.com>
2018-08-26 01:13:26 -0700

    Create home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,186 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { StaticQuery, graphql, Link } from 'gatsby'
+import Img from 'gatsby-image'
+import styled from 'styled-components'
 
 import Layout from '../components/layout'
+import Hero from '../components/hero'
+import FlexibleContent from '../components/flexible-content'
+
+const Container = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`
+
+const Row = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const Column = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
+  flex: 0 0 auto;
+  width: 100%;
+  @media (min-width: 768px) {
+    width: 50%;
+  }
+`
+
+const FeatureTitle = styled.h2`
+  border-bottom: 3px solid #05c6c7;
+  color: #7f7f7f;
+  padding: 1.5rem;
+  font-family: 'Josefin Sans', sans-serif;
+  text-transform: uppercase;
+`
+
+const ButtonLink = styled(Link)`
+  border: 3px solid #ffa200;
+  background-color: transparent;
+  color: black;
+  display: inline-block;
+  letter-spacing: 1px;
+  padding: 1rem;
+  text-align: center;
+  text-decoration: none;
+  text-transform: uppercase;
+  transition: all 0.15s ease-in;
+  &:hover,
+  &:focus {
+    background-color: #ffa200;
+    color: white;
+  }
+`
 
 const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  <StaticQuery
+    query={graphql`
+      query HomeQuery {
+        defaultHero: wordpressWpMedia(
+          slug: { eq: "puroflux_home_hero_sample" }
+        ) {
+          localFile {
+            childImageSharp {
+              fluid(maxHeight: 420) {
+                ...GatsbyImageSharpFluid_withWebp_tracedSVG
+              }
+            }
+          }
+        }
+        homeHero: wordpressWpMedia(slug: { eq: "puroflux_home_hero_pf_4060" }) {
+          localFile {
+            childImageSharp {
+              fluid(maxHeight: 420) {
+                ...GatsbyImageSharpFluid_withWebp_tracedSVG
+              }
+            }
+          }
+        }
+        page: wordpressPage(slug: { eq: "home" }) {
+          acf {
+            content
+            featured_content
+            featured_image {
+              localFile {
+                childImageSharp {
+                  fluid(maxWidth: 768) {
+                    ...GatsbyImageSharpFluid_withWebp_tracedSVG
+                  }
+                }
+              }
+            }
+            layouts_page {
+              __typename
+              ... on WordPressAcf_full_width_content {
+                content
+                link {
+                  title
+                  url
+                  target
+                }
+              }
+              ... on WordPressAcf_split_content {
+                content_split
+                left_background_image {
+                  localFile {
+                    childImageSharp {
+                      fluid(maxWidth: 768) {
+                        ...GatsbyImageSharpFluid_withWebp_tracedSVG
+                      }
+                    }
+                  }
+                }
+                left_title
+                left_content
+                left_link {
+                  title
+                  url
+                  target
+                }
+                right_background_image {
+                  localFile {
+                    childImageSharp {
+                      fluid(maxWidth: 768) {
+                        ...GatsbyImageSharpFluid_withWebp_tracedSVG
+                      }
+                    }
+                  }
+                }
+                right_title
+                right_content
+                right_link {
+                  title
+                  url
+                  target
+                }
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <Layout>
+        <Hero
+          isHome
+          html={data.page.acf.content}
+          homeHero={data.homeHero.localFile.childImageSharp.fluid}
+          defHero={data.defaultHero.localFile.childImageSharp.fluid}
+        />
+        <Container>
+          <Row>
+            <Column>
+              {data.page.acf.featured_image && (
+                <Img
+                  fluid={
+                    data.page.acf.featured_image.localFile.childImageSharp.fluid
+                  }
+                />
+              )}
+            </Column>
+            <Column>
+              <FeatureTitle>Featured</FeatureTitle>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: data.page.acf.featured_content,
+                }}
+              />
+              <ButtonLink to={`/gallery`}>View Gallery</ButtonLink>
+            </Column>
+          </Row>
+        </Container>
+        {data.page.acf.layouts_page && (
+          <FlexibleContent layouts={data.page.acf.layouts_page} />
+        )}
+      </Layout>
+    )}
+  />
 )
 
 export default IndexPage
