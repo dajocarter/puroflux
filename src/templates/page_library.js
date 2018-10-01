2629111ec643c13c88075bb22248512f8f8f512b
David Carter <dajocarter@gmail.com>
2018-09-30 19:36:30 -0700

    Update library template

--- src/templates/page_library.js
+++ src/templates/page_library.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { graphql } from 'gatsby'
+import { StaticQuery, graphql } from 'gatsby'
 import styled from 'styled-components'
 
 import Layout from '../components/layout'
@@ -11,29 +11,100 @@ const Content = styled.div`
   padding: 1.45rem 1.0875rem;
 `
 
-const LibraryTemplate = ({ data }) => (
-  <Layout>
-    <Hero html={data.page.acf.content} links={data.page.acf.buttons} />
-    <Content dangerouslySetInnerHTML={{ __html: data.page.content }} />
-  </Layout>
-)
+const Library = styled.div`
+  background-color: black;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: flex-start;
+  align-items: flex-start;
+`
 
-export default LibraryTemplate
+const LibraryGroup = styled.div`
+  flex: 0 0 auto;
+  h2 {
+    color: white;
+  }
+  ul,
+  a {
+    color: #ffa200;
+    text-decoration: none;
+  }
+`
 
-export const query = graphql`
-  query LibraryQuery($id: String!) {
-    page: wordpressPage(id: { eq: $id }) {
-      content
-      acf {
-        content
-        buttons {
-          button_link {
-            title
-            target
-            url
+const LibraryTemplate = () => (
+  <StaticQuery
+    query={graphql`
+      query LibraryQuery {
+        page: wordpressPage(template: { eq: "page_library.php" }) {
+          content
+          acf {
+            content
+            buttons {
+              button_link {
+                title
+                target
+                url
+              }
+            }
+            file_groups {
+              group_name
+              files {
+                file {
+                  wordpress_id
+                  title
+                  url {
+                    source_url
+                  }
+                }
+              }
+              sub_groups {
+                group_name
+                files {
+                  file {
+                    wordpress_id
+                    title
+                    url {
+                      source_url
+                    }
+                  }
+                }
+              }
+            }
           }
         }
       }
-    }
-  }
-`
+    `}
+    render={data => (
+      <Layout>
+        <Hero html={data.page.acf.content} links={data.page.acf.buttons} />
+        {data.page.content && (
+          <Content dangerouslySetInnerHTML={{ __html: data.page.content }} />
+        )}
+        <Library>
+          {data.page.acf.file_groups.map((group, i) => (
+            <LibraryGroup key={i}>
+              <h2>{group.group_name}</h2>
+              <ul>
+                {group.files.map(item => (
+                  <li key={item.file.wordpress_id}>
+                    <a
+                      href={`${process.env.SOURCE_URL}${
+                        item.file.url.source_url
+                      }`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {item.file.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </LibraryGroup>
+          ))}
+        </Library>
+      </Layout>
+    )}
+  />
+)
+
+export default LibraryTemplate
