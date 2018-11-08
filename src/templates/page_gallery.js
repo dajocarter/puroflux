9c73eb07ee6f9832313d85f991601ea443c40cc9
David Carter <dajocarter@gmail.com>
2018-11-08 15:16:56 -0800

    Use a carousel for image gallery

--- src/templates/page_gallery.js
+++ src/templates/page_gallery.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
+import {Container, Row, Col} from 'react-bootstrap'
+import Carousel from 'react-bootstrap/lib/Carousel'
 import styled from 'styled-components'
 
 import Layout from '../components/layout'
 import Hero from '../components/hero'
+
+const Main = styled(Container)`
+	padding: 45px 15px;
+`
 
 const Content = styled.div`
   margin: 0 auto;
@@ -44,13 +50,28 @@ const GalleryTemplate = () => (
     render={data => (
       <Layout>
         <Hero html={data.page.acf.content} links={data.page.acf.buttons} />
-        {data.page.content && (
-          <Content dangerouslySetInnerHTML={{ __html: data.page.content }} />
-        )}
-        {data.page.acf.gallery &&
-          data.page.acf.gallery.map(img => (
-            <Img key={img.id} fluid={img.localFile.childImageSharp.fluid} />
-          ))}
+				<Main>
+        	<Row>
+						<Col xs={12}>
+							{data.page.content && (
+          			<Content dangerouslySetInnerHTML={{ __html: data.page.content }} />
+        			)}
+						</Col>
+					</Row>
+					<Row>
+						<Col xs={12}>
+							{data.page.acf.gallery &&
+								<Carousel>
+									{data.page.acf.gallery.map(img => (
+										<Carousel.Item key={img.id}>
+											<Img fluid={img.localFile.childImageSharp.fluid} />
+										</Carousel.Item>
+									))}
+								</Carousel>
+							}
+						</Col>
+					</Row>
+				</Main>
       </Layout>
     )}
   />
