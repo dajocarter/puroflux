6a3ff571c7c5f83327d95dc0e0c669486dc4d211
David Carter <dajocarter@gmail.com>
2018-12-26 20:27:56 -0800

    Duplicate contactform for newsletter signup and 3D requests

---
+++ src/components/forms/Request3dForm.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import { FormGroup, FormControl, FormLabel, Button } from 'react-bootstrap'
+
+import 'bootstrap/dist/css/bootstrap.css'
+
+export default class Form extends Component {
+  render () {
+    return (
+      <form
+        name='contact'
+        method='POST'
+        action='https://formspree.io/dajocarter@gmail.com'
+      >
+        <FormGroup controlId='name'>
+          <FormLabel>Name</FormLabel>
+          <FormControl type='text' name='name' placeholder='Your name' />
+        </FormGroup>
+        <FormGroup controlId='email'>
+          <FormLabel>Email</FormLabel>
+          <FormControl type='email' name='_replyto' placeholder='Your email' />
+        </FormGroup>
+        <FormGroup controlId='message'>
+          <FormLabel>Message</FormLabel>
+          <FormControl as='textarea' placeholder='Your message' />
+        </FormGroup>
+        <FormGroup controlId='location'>
+          <FormLabel>Location</FormLabel>
+          <FormControl type='text' placeholder='Your location' />
+        </FormGroup>
+        <input
+          type='hidden'
+          name='_subject'
+          value='New Contact Form Submission'
+        />
+        <input type='text' name='_gotcha' style={{ display: 'none' }} />
+        <Button type='submit' variant='light'>
+          Submit
+        </Button>
+      </form>
+    )
+  }
+}
