541c810ff7f37723ee5f0e04358ea2cf2402444f
dajocarter <dajocarter@gmail.com>
2022-08-24 17:23:13 -0700

    install styled-components and other libraries

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
 import '../styles/globals.css'
+import type { ReactElement, ReactNode } from 'react'
+import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
+import { ThemeProvider, DefaultTheme } from 'styled-components'
+import GlobalStyle from '../components/globalstyles'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const theme = {
+  primary: '#09A198',
+  secondary: '#ffa200',
+  alt: '#000000',
+  body: '#7F7F7F'
 }
 
-export default MyApp
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page)
+  
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      {getLayout(<Component {...pageProps} />)}
+    </ThemeProvider>
+  )
+}
