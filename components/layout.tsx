997e273e9c04af915b07e7025234052dcf32523f
dajocarter <dajocarter@gmail.com>
2022-08-26 13:27:52 -0700

    create a layout component

---
+++ components/layout.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+import Header from "./header";
+import Footer from "./footer";
+
+export default function Layout (props) {
+  return (
+    <>
+      <Header {...props.header} />
+      <Main>{props.children}</Main>
+      <Footer {...props.footer} />
+    </>
+  )
+}
+
+const Main = styled.main`
+  font-family: 'Lato', sans-serif;
+`
